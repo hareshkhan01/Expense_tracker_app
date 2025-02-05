6d03bc49a7e10b8fc1f09f2971a6d0dca0b1a22c
Haresh Khan <hareshkhan060@gmail.com>
2025-02-05 16:13:46 +0530

    set the gemini api key and monodb

--- src/config/config.ts
+++ src/config/config.ts
@@ -3,9 +3,9 @@ conf();
 
 const _config = {
     port:process.env.PORT || 3000,
-    apiKey: process.env.API_KEY || '',
-    mongoString: process.env.MONGO_STRING || '',
-    nodeEnv: process.env.NODE_ENV || ''
+    apiKey: process.env.API_KEY || 'AIzaSyC2oepjp7tbKgUGlCpV0aNP8ZAKcQ1c_4g', //Gemini API key
+    mongoString: process.env.MONGO_STRING || 'mongodb://127.0.0.1:27017/',
+    nodeEnv: process.env.NODE_ENV || 'development'
 };
 
 export const config = Object.freeze(_config);
