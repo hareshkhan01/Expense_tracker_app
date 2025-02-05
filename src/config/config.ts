ad686108ec329df7d30584815b4c6e926353fd20
hareshkhan01 <124094308+hareshkhan01@users.noreply.github.com>
2025-02-06 02:23:31 +0530

    Update config.ts

--- src/config/config.ts
+++ src/config/config.ts
@@ -3,9 +3,9 @@ conf();
 
 const _config = {
     port:process.env.PORT || 3000,
-    apiKey: process.env.API_KEY || 'AIzaSyC2oepjp7tbKgUGlCpV0aNP8ZAKcQ1c_4g', //Gemini API key
-    mongoString: process.env.MONGO_STRING || 'mongodb://127.0.0.1:27017/',
-    nodeEnv: process.env.NODE_ENV || 'development'
+    apiKey: process.env.API_KEY || '', //Gemini API key
+    mongoString: process.env.MONGO_STRING || '',
+    nodeEnv: process.env.NODE_ENV || ''
 };
 
 export const config = Object.freeze(_config);
