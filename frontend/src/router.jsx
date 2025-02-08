92bc1bd77c9468039cfcc29f9e68e2b037eb35b3
debjyoti sarkar <debjyotisarkar929@gmail.com>
2025-02-09 03:03:43 +0530

    addd homepage and expense to router

--- frontend/src/router.jsx
+++ frontend/src/router.jsx
@@ -1,10 +1,20 @@
 import { createBrowserRouter, Navigate } from 'react-router-dom';
 import HomePage from './pages/HomePage';
+import Dashboard from './pages/DashboardPages';
+import AddExpensePage from './pages/AddExpensePage';
 const router = createBrowserRouter([
     {
         path: '/',
         element: <HomePage />,
     },
+    {
+        path: '/dashboard/home',
+        element: <Dashboard />
+    },
+    {
+        path:'/addExpense',
+        element: <AddExpensePage/>
+    }
   
 ])
 
